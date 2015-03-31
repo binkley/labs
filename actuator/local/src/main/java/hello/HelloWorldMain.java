8eee7f247fb2ea034aacfd72c837b08f543a8641
Brian Oxley <boxley@thoughtworks.com>
2015-03-31 18:52:47 -0400

    Simpler way for Swagger.

--- actuator/local/src/main/java/hello/HelloWorldMain.java
+++ actuator/local/src/main/java/hello/HelloWorldMain.java
@@ -1,5 +1,6 @@
 package hello;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -7,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableSwagger
 @SpringBootApplication
 public class HelloWorldMain {
     public static void main(final String... args) {
