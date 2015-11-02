f795e2b0fc0caf6eba489f2ee64a9c276d2f56dc
Brian Oxley <boxley@thoughtworks.com>
2015-11-01 19:37:20 -0500

    Test JGit.

---
+++ version-diff/src/main/java/hm/binkley/labs/FindCommits.java
@@ -0,0 +1,68 @@
+package hm.binkley.labs;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.PathSuffixFilter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+
+/**
+ * {@code FindCommits} <strong>needs documentation</strong>.
+ *
+ * @author <a href="mailto:boxley@thoughtworks.com">Brian Oxley</a>
+ * @todo Needs documentation
+ */
+public final class FindCommits {
+    private FindCommits() {}
+
+    public static void writeOutCommits(final Repository repo,
+            final ObjectId commitId, final Consumer<TreeWalk> configure,
+            final IOFunction<String, Path> out)
+            throws IOException {
+        try (final RevWalk revWalk = new RevWalk(repo)) {
+            final RevCommit commit = revWalk.parseCommit(commitId);
+            final RevTree tree = commit.getTree();
+            try (final TreeWalk treeWalk = new TreeWalk(repo)) {
+                treeWalk.addTree(tree);
+                treeWalk.setRecursive(true);
+                configure.accept(treeWalk);
+
+                if (!treeWalk.next())
+                    throw new IOException("No Java");
+                else {
+                    final ObjectId objectId = treeWalk.getObjectId(0);
+                    final ObjectLoader loader = repo.open(objectId);
+                    try (final OutputStream src = new FileOutputStream(
+                            out.apply(treeWalk.getPathString()).toFile())) {
+                        loader.copyTo(src);
+                    }
+                }
+            }
+
+            revWalk.dispose();
+        }
+    }
+
+    public static void findCommits(final Repository repo,
+            final IOConsumer<RevCommit> process)
+            throws IOException {
+        final Ref head = repo.getRef("refs/heads/master");
+        try (final RevWalk walk = new RevWalk(repo)) {
+            final RevCommit commit = walk.parseCommit(head.getObjectId());
+            walk.markStart(commit);
+            for (final RevCommit rev : walk)
+                process.accept(rev);
+            walk.dispose();
+        }
+    }
+}
--- version-diff/src/main/java/hm/binkley/labs/VersionDiffPOC.java
+++ version-diff/src/main/java/hm/binkley/labs/VersionDiffPOC.java
@@ -2,13 +2,8 @@ package hm.binkley.labs;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ObjectLoader;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevTree;
-import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.filter.PathSuffixFilter;
@@ -18,10 +13,8 @@ import javax.tools.JavaCompiler;
 import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOError;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -36,6 +29,8 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
+import static hm.binkley.labs.FindCommits.findCommits;
+import static hm.binkley.labs.FindCommits.writeOutCommits;
 import static hm.binkley.labs.IORunnable.rethrow;
 import static java.lang.Runtime.getRuntime;
 import static java.lang.System.out;
@@ -82,6 +77,7 @@ public final class VersionDiffPOC {
         try (final StandardJavaFileManager files = javac
                 .getStandardFileManager(null, null, null)) {
             findCommits(repo, commit -> writeOutCommits(repo, commit.getId(),
+                    VersionDiffPOC::configureTreeWalk,
                     revPath -> compile(buildDir, commit, commits, files,
                             revPath)));
         }
@@ -110,17 +106,21 @@ public final class VersionDiffPOC {
         });
     }
 
+    private static void configureTreeWalk(final TreeWalk treeWalk) {
+        treeWalk.setFilter(PathSuffixFilter.create(".java"));
+    }
+
     private static Path compile(final Path buildDir, final RevCommit commit,
             final List<CompiledCommit> commits,
             final StandardJavaFileManager files, final String revPath)
             throws IOException {
         final Path relativeSrcPath = relativeSrcDir
                 .relativize(Paths.get(revPath));
-        final String className = toJavaName(relativeSrcPath);
         final Path srcFile = buildDir.resolve(relativeSrcPath);
         mkdirs(srcFile.getParent());
-        final CompiledCommit compiledCommit = CompiledCommit
-                .of(commit, loadClasses(buildDir, files, className, srcFile));
+        final CompiledCommit compiledCommit = CompiledCommit.of(commit,
+                loadClasses(buildDir, files, toJavaName(relativeSrcPath),
+                        srcFile));
         commits.add(compiledCommit);
         return srcFile;
     }
@@ -209,7 +209,7 @@ public final class VersionDiffPOC {
 
             writeAndCommit(git, barFile,
                     "Fake change - first commit ignored?", "package scratch;",
-                    "public final class Bar {}");   
+                    "public final class Bar {}");
         }
     }
 
@@ -231,45 +231,5 @@ public final class VersionDiffPOC {
             git.commit().
                     setMessage(commitMessage).
                     call();
-    }
-
-    private static void findCommits(final Repository repo,
-            final IOConsumer<RevCommit> process)
-            throws IOException {
-        final Ref head = repo.getRef("refs/heads/master");
-        try (final RevWalk walk = new RevWalk(repo)) {
-            final RevCommit commit = walk.parseCommit(head.getObjectId());
-            walk.markStart(commit);
-            for (final RevCommit rev : walk)
-                process.accept(rev);
-            walk.dispose();
-        }
-    }
-
-    private static void writeOutCommits(final Repository repo,
-            final ObjectId commitId, final IOFunction<String, Path> out)
-            throws IOException {
-        try (final RevWalk revWalk = new RevWalk(repo)) {
-            final RevCommit commit = revWalk.parseCommit(commitId);
-            final RevTree tree = commit.getTree();
-            try (final TreeWalk treeWalk = new TreeWalk(repo)) {
-                treeWalk.addTree(tree);
-                treeWalk.setRecursive(true);
-                treeWalk.setFilter(PathSuffixFilter.create(".java"));
-
-                if (!treeWalk.next())
-                    throw new IOException("No Java");
-                else {
-                    final ObjectId objectId = treeWalk.getObjectId(0);
-                    final ObjectLoader loader = repo.open(objectId);
-                    try (final OutputStream src = new FileOutputStream(
-                            out.apply(treeWalk.getPathString()).toFile())) {
-                        loader.copyTo(src);
-                    }
-                }
-            }
-
-            revWalk.dispose();
-        }
     }
 }
