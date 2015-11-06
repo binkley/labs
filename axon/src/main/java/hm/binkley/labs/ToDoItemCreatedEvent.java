dd51bfb4074274c7634fc4543c8761d2aa5373bc
Brian Oxley <boxley@thoughtworks.com>
2015-11-06 06:48:57 -0500

    Fix dep analysis.

--- axon/src/main/java/hm/binkley/labs/ToDoItemCreatedEvent.java
+++ axon/src/main/java/hm/binkley/labs/ToDoItemCreatedEvent.java
@@ -3,9 +3,13 @@ package hm.binkley.labs;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.annotation.Nonnull;
+
 @RequiredArgsConstructor
 @ToString
 public class ToDoItemCreatedEvent {
+    @Nonnull
     private final String name;
+    @Nonnull
     private final String data;
 }
