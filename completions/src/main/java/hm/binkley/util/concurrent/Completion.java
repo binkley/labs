9c5453327f7627198fd92aed1510243546b83f96
Brian Oxley <boxley@thoughtworks.com>
2015-11-16 08:21:10 -0500

    Docs and more testing.

--- completions/src/main/java/hm/binkley/util/concurrent/Completion.java
+++ completions/src/main/java/hm/binkley/util/concurrent/Completion.java
@@ -4,6 +4,8 @@ import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -16,31 +18,74 @@ import static java.lang.Thread.currentThread;
 import static lombok.AccessLevel.PRIVATE;
 
 /**
- * {@code Completion} <strong>needs documentation</strong>.
+ * {@code Completion} is a retryable {@code Callable} wrapper.
  *
  * @author <a href="mailto:boxley@thoughtworks.com">Brian Oxley</a>
- * @todo Needs documentation
  */
 @EqualsAndHashCode
 @RequiredArgsConstructor(access = PRIVATE)
 @ToString
 public final class Completion<T>
         implements Callable<T> {
+    /** The callable values, if available. */
+    @Nullable
     public final T value;
+    /** The callable failure, if available. */
+    @Nullable
     public final Exception failure;
 
-    public static <T> Completion<T> valued(final T value) {
+    /**
+     * Creates a new {@code Completion} with the given return
+     * <var>value</var>.
+     *
+     * @param value the value
+     * @param <T> the value type
+     *
+     * @return the new completion, never {@code null}
+     */
+    @Nonnull
+    public static <T> Completion<T> valued(@Nullable final T value) {
         return new Completion<>(value, null);
     }
 
-    public static <T> Completion<T> failed(final Exception e) {
+    /**
+     * Creates a new {@code Completion} with the given failure.
+     *
+     * @param e the failure, never {@code null}
+     * @param <T> the value type
+     *
+     * @return the new completion, never {@code null}
+     */
+    @Nonnull
+    public static <T> Completion<T> failed(@Nonnull final Exception e) {
         return new Completion<>(null, e);
     }
 
-    public static <T> T eventually(final Callable<T> callable,
-            final Predicate<Completion<T>> isDone,
-            final LongConsumer peekDelay, final TimeUnit unit,
-            final long firstDelay, final long... restOfDelays)
+    /**
+     * Attempts to complete the given <var>callable</var> until done or timeed
+     * out.
+     *
+     * @param callable the underlying callable to complete, never {@code
+     * null}
+     * @param isDone the measure of when the callable is done, never {@code
+     * null}
+     * @param peekDelay the peek function for each retry, never {@code null}
+     * @param unit the delay time unit, never {@code null}
+     * @param firstDelay the amount of first delay, use 0 to try immediately
+     * @param restOfDelays the amount of subsequent delays
+     * @param <T> the  value type
+     *
+     * @return the callable result, if available
+     *
+     * @throws Exception if the call fails
+     * @see #eventually(Callable, Predicate, TimeUnit, long, long...)
+     */
+    @Nullable
+    public static <T> T eventually(@Nonnull final Callable<T> callable,
+            @Nonnull final Predicate<Completion<T>> isDone,
+            @Nonnull final LongConsumer peekDelay,
+            @Nonnull final TimeUnit unit, final long firstDelay,
+            final long... restOfDelays)
             throws Exception {
         return LongStream.of(delays(firstDelay, restOfDelays)).
                 peek(peekDelay).
@@ -51,14 +96,36 @@ public final class Completion<T>
                 call();
     }
 
-    public static <T> T eventually(final Callable<T> callable,
-            final Predicate<Completion<T>> isDone, final TimeUnit unit,
-            final long firstDelay, final long... delays)
+    /**
+     * Attempts to complete the given <var>callable</var> until done or timeed
+     * out.
+     *
+     * @param callable the underlying callable to complete, never {@code
+     * null}
+     * @param isDone the measure of when the callable is done, never {@code
+     * null}
+     * @param unit the delay time unit, never {@code null}
+     * @param firstDelay the amount of first delay, use 0 to try immediately
+     * @param restOfDelays the amount of subsequent delays
+     * @param <T> the  value type
+     *
+     * @return the callable result, if available
+     *
+     * @throws Exception if the call fails
+     * @see #eventually(Callable, Predicate, LongConsumer, TimeUnit, long,
+     * long...)
+     */
+    @Nullable
+    public static <T> T eventually(@Nonnull final Callable<T> callable,
+            @Nonnull final Predicate<Completion<T>> isDone,
+            @Nonnull final TimeUnit unit, final long firstDelay,
+            final long... restOfDelays)
             throws Exception {
         return eventually(callable, isDone, delay -> {}, unit, firstDelay,
-                delays);
+                restOfDelays);
     }
 
+    @Nullable
     @Override
     public T call()
             throws Exception {
@@ -68,7 +135,7 @@ public final class Completion<T>
     }
 
     private static long[] delays(final long firstDelay,
-            final long[] restOfDelays) {
+            final long... restOfDelays) {
         final long[] delays = new long[1 + restOfDelays.length];
         delays[0] = firstDelay;
         arraycopy(restOfDelays, 0, delays, 1, restOfDelays.length);
--- completions/src/test/java/hm/binkley/util/concurrent/CompletionTest.java
+++ completions/src/test/java/hm/binkley/util/concurrent/CompletionTest.java
@@ -1,6 +1,7 @@
 package hm.binkley.util.concurrent;
 
 import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
 import org.junit.Test;
 
 import java.io.PrintStream;
@@ -11,33 +12,42 @@ import java.util.function.Supplier;
 import static hm.binkley.util.concurrent.Completion.eventually;
 import static java.lang.System.out;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static lombok.AccessLevel.PRIVATE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CompletionTest {
     @Test
     public void shouldComplete()
             throws Exception {
-        assertThat(eventually(new NthAttempt(NeedsRetryException::new, 3),
+        assertThat(eventually(new NthAttempt(3, NeedsRetryException::new, 3),
                 CompletionTest::isDone, CompletionTest::peekDelay,
                 MILLISECONDS, 0, 10, 10, 20)).
                 isEqualTo(3);
     }
 
+    @Test
+    public void shouldCompleteWithNullValue()
+            throws Exception {
+        assertThat(
+                eventually(new NthAttempt(null, NeedsRetryException::new, 3),
+                        CompletionTest::isDone, CompletionTest::peekDelay,
+                        MILLISECONDS, 0, 10, 10, 20)).
+                isNull();
+    }
+
     @Test(expected = TimeoutException.class)
     public void shouldTimeout()
             throws Exception {
-        assertThat(eventually(new NthAttempt(NeedsRetryException::new, 3),
+        eventually(new NthAttempt(3, NeedsRetryException::new, 3),
                 CompletionTest::isDone, CompletionTest::peekDelay,
-                MILLISECONDS, 0, 10)).
-                isEqualTo(3);
+                MILLISECONDS, 0, 10);
     }
 
     @Test(expected = WentWrongException.class)
     public void shouldFail()
             throws Exception {
-        assertThat(eventually(new NthAttempt(WentWrongException::new, 3),
-                CompletionTest::isDone, MILLISECONDS, 0)).
-                isEqualTo(3);
+        eventually(new NthAttempt(3, WentWrongException::new, 3),
+                CompletionTest::isDone, MILLISECONDS, 0);
     }
 
     private static <T> boolean isDone(final Completion<T> completion) {
@@ -56,22 +66,19 @@ public class CompletionTest {
     private static final class WentWrongException
             extends Exception {}
 
+    @RequiredArgsConstructor(access = PRIVATE)
     private static final class NthAttempt
             implements Callable<Integer> {
+        private final Integer value;
         private final Supplier<Exception> failure;
         private final int n;
         private int attempt;
-
-        private NthAttempt(final Supplier<Exception> failure, final int n) {
-            this.failure = failure;
-            this.n = n;
-        }
 
         @Override
         public Integer call()
                 throws Exception {
             if (n <= ++attempt)
-                return 3;
+                return value;
             throw failure.get();
         }
     }
