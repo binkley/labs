c0679f05327c6c4a6f10316713b8edbb1c3bc036
Brian Oxley <boxley@thoughtworks.com>
2015-11-16 09:50:31 -0500

    Cleaner test code.

--- completions/src/test/java/hm/binkley/util/concurrent/CompletionTest.java
+++ completions/src/test/java/hm/binkley/util/concurrent/CompletionTest.java
@@ -1,10 +1,12 @@
 package hm.binkley.util.concurrent;
 
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -14,13 +16,18 @@ import java.util.function.Supplier;
 import static hm.binkley.util.concurrent.CompletionAssertJ.given;
 import static java.util.Arrays.asList;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static lombok.AccessLevel.PRIVATE;
 
 public class CompletionTest {
     @Test
     public void shouldComplete()
             throws Exception {
-        given(new NthAttempt(3, NeedsRetryException::new, 3)).
+        final NthAttempt callable = NthAttempt.builder().
+                attempting(3).
+                returning(3).
+                throwing(NeedsRetryException::new).
+                build();
+
+        given(callable).
                 whenRetrying(MILLISECONDS, 0, 10, 10, 20).
                 thenAssert(CompletionTest::isDone).
                 isEqualTo(3);
@@ -29,7 +36,13 @@ public class CompletionTest {
     @Test
     public void shouldCompleteWithNullValue()
             throws Exception {
-        given(new NthAttempt(null, NeedsRetryException::new, 3)).
+        final NthAttempt callable = NthAttempt.builder().
+                attempting(3).
+                returning(null).
+                throwing(NeedsRetryException::new).
+                build();
+
+        given(callable).
                 whenRetrying(MILLISECONDS, 0, 10, 10, 20).
                 thenAssert(CompletionTest::isDone).
                 isNull();
@@ -38,9 +51,14 @@ public class CompletionTest {
     @Test
     public void shouldPeekAtDelays()
             throws Exception {
+        final NthAttempt callable = NthAttempt.builder().
+                attempting(3).
+                returning(null).
+                throwing(NeedsRetryException::new).
+                build();
         final List<Long> delays = new ArrayList<>(4);
 
-        given(new NthAttempt(null, NeedsRetryException::new, 3)).
+        given(callable).
                 peeking(delays::add).
                 whenRetrying(MILLISECONDS, 0, 10, 10, 20).
                 thenAssert(CompletionTest::isDone);
@@ -51,7 +69,12 @@ public class CompletionTest {
     @Test(expected = TimeoutException.class)
     public void shouldTimeout()
             throws Exception {
-        given(new NthAttempt(3, NeedsRetryException::new, 3)).
+        final NthAttempt callable = NthAttempt.builder().
+                attempting(3).
+                throwing(NeedsRetryException::new).
+                build();
+
+        given(callable).
                 whenRetrying(MILLISECONDS, 0, 10).
                 thenAssert(CompletionTest::isDone);
     }
@@ -59,7 +82,12 @@ public class CompletionTest {
     @Test(expected = CheckedWentWrongException.class)
     public void shouldFailWithChecked()
             throws Exception {
-        given(new NthAttempt(3, CheckedWentWrongException::new, 3)).
+        final NthAttempt callable = NthAttempt.builder().
+                attempting(2).
+                throwing(CheckedWentWrongException::new).
+                build();
+
+        given(callable).
                 whenRetrying(MILLISECONDS, 0).
                 thenAssert(CompletionTest::isDone);
     }
@@ -67,7 +95,12 @@ public class CompletionTest {
     @Test(expected = UncheckedWentWrongException.class)
     public void shouldFailWithUnchecked()
             throws Exception {
-        given(new NthAttempt(3, UncheckedWentWrongException::new, 3)).
+        final NthAttempt callable = NthAttempt.builder().
+                attempting(2).
+                throwing(UncheckedWentWrongException::new).
+                build();
+
+        given(callable).
                 whenRetrying(MILLISECONDS, 0).
                 thenAssert(CompletionTest::isDone);
     }
@@ -88,20 +121,23 @@ public class CompletionTest {
     private static final class UncheckedWentWrongException
             extends RuntimeException {}
 
-    @RequiredArgsConstructor(access = PRIVATE)
+    @Builder
     private static final class NthAttempt
             implements Callable<Integer> {
-        private final Integer value;
-        private final Supplier<Exception> failure;
-        private final int n;
+        private final int attempting;
+        @Nullable
+        private final Integer returning;
+        @Nonnull
+        private final Supplier<Exception> throwing;
+
         private int attempt;
 
         @Override
         public Integer call()
                 throws Exception {
-            if (n <= ++attempt)
-                return value;
-            throw failure.get();
+            if (attempting <= ++attempt)
+                return returning;
+            throw throwing.get();
         }
     }
 }
