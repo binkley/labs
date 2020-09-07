17e20877f7ba90745409a19c71b5b7f712d1a797
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-09-07 05:50:12 -0500

    Drop unused modules

--- axon/src/main/java/hm/binkley/labs/todo/api/TodoCreatedEvent.java
+++
@@ -1,9 +0,0 @@
-package hm.binkley.labs.todo.api;
-
-import lombok.Data;
-
-@Data(staticConstructor = "of")
-public final class TodoCreatedEvent {
-    private final String name;
-    private final String content;
-}
--- reveno/src/main/java/hm/binkley/labs/reveno/basic/BalanceChangedEvent.java
+++
@@ -1,8 +0,0 @@
-package hm.binkley.labs.reveno.basic;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class BalanceChangedEvent {
-    public final long accountId;
-}
